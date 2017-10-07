de15c1ec2ac073c8a35d2a4aadf601873ec3d882
Lex <alexreferal@gmail.com>
2017-10-07 18:22:21 +0200

    service gotov

--- src/main/java/program/bookcase/service/BookService.java
+++ src/main/java/program/bookcase/service/BookService.java
@@ -1,5 +1,9 @@
 package program.bookcase.service;
 
+import program.bookcase.model.Books;
+
+import java.util.List;
+
 /*
 *Lex
 *
@@ -7,4 +11,30 @@ package program.bookcase.service;
 *18:01
 */
 public interface BookService {
+    //Добавление книги
+    public void addBook(Books book);
+
+    //Изменение
+    public void updateBook(Books book);
+
+    //Удаление
+    public void removeBook(Books book);
+
+    // Метод чтения книги
+    public void readBook(Books book);
+
+    // Метод поиска книги по названию
+    public List<Books> searchBookByName(String title);
+
+    // Метод поиска книги по году
+    public List<Books> searchBookByYear(int printYear);
+
+    // Метод поиска книги по прочтению
+    public List<Books> searchBookByRead(boolean readAlready);
+
+    // Метод вывода списка книг
+    public List<Books> listBooks();
+
+    // Получение книги по id
+    public Books getBookById(int id);
 }
--- src/main/java/program/bookcase/service/BookServiceImpl.java
+++ src/main/java/program/bookcase/service/BookServiceImpl.java
@@ -1,10 +1,77 @@
 package program.bookcase.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import program.bookcase.dao.BookDao;
+import program.bookcase.model.Books;
+
+import java.util.List;
+
 /*
 *Lex
 *
 *07.10.2017
 *18:02
 */
-public class BookServiceImpl {
+@Service
+public class BookServiceImpl implements BookService {
+    private BookDao bookDao;
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @Override
+    @Transactional
+    public void addBook(Books book) {
+        this.bookDao.addBook(book);
+    }
+
+    @Override
+    @Transactional
+    public void updateBook(Books book) {
+        this.bookDao.updateBook(book);
+    }
+
+    @Override
+    @Transactional
+    public void removeBook(Books book) {
+        this.bookDao.removeBook(book);
+    }
+
+    @Override
+    @Transactional
+    public void readBook(Books book) {
+        this.bookDao.readBook(book);
+    }
+
+    @Override
+    @Transactional
+    public List<Books> searchBookByName(String title) {
+        return this.bookDao.searchBookByName(title);
+    }
+
+    @Override
+    @Transactional
+    public List<Books> searchBookByYear(int printYear) {
+        return this.bookDao.searchBookByYear(printYear);
+    }
+
+    @Override
+    @Transactional
+    public List<Books> searchBookByRead(boolean readAlready) {
+        return this.bookDao.searchBookByRead(readAlready);
+    }
+
+    @Override
+    @Transactional
+    public List<Books> listBooks() {
+        return this.bookDao.listBooks();
+    }
+
+    @Override
+    @Transactional
+    public Books getBookById(int id) {
+        return this.bookDao.getBookById(id);
+    }
 }
