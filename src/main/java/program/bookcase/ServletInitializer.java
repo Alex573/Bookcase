b9c9074ee224ec1f7cc4a2b615e5f3491010d6aa
Lex <alexreferal@gmail.com>
2017-10-07 17:16:10 +0200

    Start

---
+++ src/main/java/program/bookcase/ServletInitializer.java
@@ -0,0 +1,13 @@
+package program.bookcase;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BookcaseApplication.class);
+    }
+
+}
