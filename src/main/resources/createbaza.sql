7a023ed68148d280acef808ca265819f5db7efdb
Lex <alexreferal@gmail.com>
2017-10-09 17:27:16 +0200

    web2

--- src/main/resources/createbaza.sql
+++ src/main/resources/createbaza.sql
@@ -1,40 +1,42 @@
-USE test;
+USE `test`;
 
-DROP TABLE IF EXISTS `book`;
+DROP TABLE IF EXISTS `test`.`book`;
+CREATE TABLE `test`.`book` (
+  `id` INT UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NULL,
+  `description` VARCHAR(255) NULL,
+  `author` VARCHAR(100) NULL,
+  `isbn` VARCHAR(20) NULL,
+  `printYear` INT NULL,
+  `readAlready` TINYINT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+COMMENT = 'book_table';
 
-CREATE TABLE `book` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `author` varchar(100) NOT NULL,
-  `isbn` varchar(20) NOT NULL,
-  `print_year` int(11) NOT NULL,
-  `read_already` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Повести Белкина ','В 1830 году Россию захватила вспышка холеры. Дорога к Москве была перекрыта из-за карантина,','Пушкин','689-5-91045-983-4','2017','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Евгений Онегин','«Евгений Онегин»  самое известное и самое значительное произведение А. С. Пушкина, вершина ','Пушкин','690-5-91045-983-4','2017','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Герой нашего времени','В романе Лермонтова соединены пять историй о молодом человеке, которого автор назвал ','Лермонтов','978-5-9691-1601-6','2017','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Маскарад','"Маскарад" - драма Михаила Юрьевича Лермонтова в четырёх действиях. Дворянин Евгений Арбенин ','Лермонтов','979-5-9691-1601-7','2017','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Демон.Сборник','В книгу великого русского поэта Михаила Юрьевича Лермонтова включены поэмы и повести ','Лермонтов','980-5-9691-1601-8','2012','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Сказка о царе Салтане','Дорогие читатели! Перед вами одна из сказок Александра Сергеевича Пушкина ','Пушкин','691-5-91045-983-4','2016','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Пиковая дама','Успех повести "Пиковая дама" начался с первого издания в 1834 году в журнале "Библиотека ','Пушкин','692-5-91045-983-4','2017','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Преступление и наказание ','Роман «Преступление и наказание» — образец психологической и ','Достоевский','990-5-9691-1601-3','2017','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Бесы','«Одно отрицание, без всякого великодушия и безо всякой силы» — произносит главный роман Николай ','Достоевский','991-5-9691-1601-6 ','2017','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Мцыри','В книге представлены стихотворения и поэма "Мцыри".','Лермонтов','981-5-9691-1601-9','2007','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Бородино: Стихотворения и поэма ','В сборник вошли избранные стихотворения великого русского поэта 18 ','Лермонтов','982-5-9691-1601-10','2004','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Княгиня Лиговская ','Роман «Княгиня Лиговская» объединил в себе традиционные черты «светской повести» ','Лермонтов','983-5-9691-1601-6','2011','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Вадим','Творческое наследие гениального русского поэта Михаила Юрьевича Лермонтова (1814-1841','Лермонтов','984-5-9691-1601-6','2011','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Барышня-крестьянка ','Барышня-крестьянка ','Пушкин','693-5-91045-983-4','2002','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Капитанская дочка ','В этот сборник вошла избранная проза Пушкина. "Повести Белкина" – цикл из пяти повестей.','Пушкин','694-5-91045-983-4','2015','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Сказка о мёртвой царевне и о семи богатырях ','Сказки Александра Сергеевича Пушкина всем знакомы и всеми ','Пушкин','695-5-91045-983-4','2016','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Братья Карамазовы ','"Братья Карамазовы" — итог творчества писателя, самое сложное, многоуровневое и ','Достоевский','987-5-9691-1601-8','2015','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Идиот','Роман «Идиот» занимает в творчестве Ф.М. Достоевского особое место. Здесь он впервые ярко и ','Достоевский','988-5-9691-1601-1','2016','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Сказка о рыбаке и рыбке. Сказка о золотом петушке ','В книгу вошли две известные сказки великого русского','Пушкин','696-5-91045-983-4','2004','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Сказка о Попе и о работнике его Балде ','Что вас ждет под обложкой: Уникальное издание чудесной сказки ','Пушкин','697-5-91045-983-4','2015','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Сказка о рыбаке и рыбке ','Дорогие читатели! Перед вами одна из сказок Александра Сергеевича Пушкина - ','Пушкин','698-5-91045-983-4','2016','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Ашик-Кериб ','Набросок сюжета восточной сказки, сделанный Михаилом Юрьевичем Лермонтовым в 1837 году, ','Лермонтов','985-5-9691-1601-6','2012','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Кавказский пленник ','Творческое наследие гениального русского поэта Михаила Юрьевича Лермонтова (1814-1841)','Лермонтов','986-5-9691-1601-6','2011','0');
-INSERT INTO `test`.`book` (`title`, `description`, `author`, `isbn`, `print_year`, `read_already`) VALUES ('Униженные и оскорбленные ','Роман "Униженные и оскорбленные" (1861) создавался Ф. М. Достоевским в эпоху ','Достоевский','989-5-9691-1601-2','2016','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Повести Белкина ','В 1830 году Россию захватила вспышка холеры. Дорога к Москве была перекрыта из-за карантина,','Пушкин','689-5-91045-983-4','2017','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Евгений Онегин','«Евгений Онегин»  самое известное и самое значительное произведение А. С. Пушкина, вершина ','Пушкин','690-5-91045-983-4','2017','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Герой нашего времени','В романе Лермонтова соединены пять историй о молодом человеке, которого автор назвал ','Лермонтов','978-5-9691-1601-6','2017','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Маскарад','"Маскарад" - драма Михаила Юрьевича Лермонтова в четырёх действиях. Дворянин Евгений Арбенин ','Лермонтов','979-5-9691-1601-7','2017','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Демон.Сборник','В книгу великого русского поэта Михаила Юрьевича Лермонтова включены поэмы и повести ','Лермонтов','980-5-9691-1601-8','2012','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Сказка о царе Салтане','Дорогие читатели! Перед вами одна из сказок Александра Сергеевича Пушкина ','Пушкин','691-5-91045-983-4','2016','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Пиковая дама','Успех повести "Пиковая дама" начался с первого издания в 1834 году в журнале "Библиотека ','Пушкин','692-5-91045-983-4','2017','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Преступление и наказание ','Роман «Преступление и наказание» — образец психологической и ','Достоевский','990-5-9691-1601-3','2017','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Бесы','«Одно отрицание, без всякого великодушия и безо всякой силы» — произносит главный роман Николай ','Достоевский','991-5-9691-1601-6 ','2017','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Мцыри','В книге представлены стихотворения и поэма "Мцыри".','Лермонтов','981-5-9691-1601-9','2007','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Бородино: Стихотворения и поэма ','В сборник вошли избранные стихотворения великого русского поэта 18 ','Лермонтов','982-5-9691-1601-10','2004','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Княгиня Лиговская ','Роман «Княгиня Лиговская» объединил в себе традиционные черты «светской повести» ','Лермонтов','983-5-9691-1601-6','2011','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Вадим','Творческое наследие гениального русского поэта Михаила Юрьевича Лермонтова (1814-1841','Лермонтов','984-5-9691-1601-6','2011','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Барышня-крестьянка ','Барышня-крестьянка ','Пушкин','693-5-91045-983-4','2002','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Капитанская дочка ','В этот сборник вошла избранная проза Пушкина. "Повести Белкина" – цикл из пяти повестей.','Пушкин','694-5-91045-983-4','2015','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Сказка о мёртвой царевне и о семи богатырях ','Сказки Александра Сергеевича Пушкина всем знакомы и всеми ','Пушкин','695-5-91045-983-4','2016','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Братья Карамазовы ','"Братья Карамазовы" — итог творчества писателя, самое сложное, многоуровневое и ','Достоевский','987-5-9691-1601-8','2015','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Идиот','Роман «Идиот» занимает в творчестве Ф.М. Достоевского особое место. Здесь он впервые ярко и ','Достоевский','988-5-9691-1601-1','2016','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Сказка о рыбаке и рыбке. Сказка о золотом петушке ','В книгу вошли две известные сказки великого русского','Пушкин','696-5-91045-983-4','2004','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Сказка о Попе и о работнике его Балде ','Что вас ждет под обложкой: Уникальное издание чудесной сказки ','Пушкин','697-5-91045-983-4','2015','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Сказка о рыбаке и рыбке ','Дорогие читатели! Перед вами одна из сказок Александра Сергеевича Пушкина - ','Пушкин','698-5-91045-983-4','2016','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Ашик-Кериб ','Набросок сюжета восточной сказки, сделанный Михаилом Юрьевичем Лермонтовым в 1837 году, ','Лермонтов','985-5-9691-1601-6','2012','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Кавказский пленник ','Творческое наследие гениального русского поэта Михаила Юрьевича Лермонтова (1814-1841)','Лермонтов','986-5-9691-1601-6','2011','0');
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUE ('Униженные и оскорбленные ','Роман "Униженные и оскорбленные" (1861) создавался Ф. М. Достоевским в эпоху ','Достоевский','989-5-9691-1601-2','2016','0');
--- src/main/webapp/WEB-INF/pages/books.jsp
+++ src/main/webapp/WEB-INF/pages/books.jsp
@@ -18,19 +18,19 @@
         .tg {
             border-collapse: collapse;
             border-spacing: 0;
-            border-color: #c991cc;
+            border-color: #ccc;
         }
 
         .tg td {
             font-family: Arial, sans-serif;
-            font-size: 12px;
+            font-size: 14px;
             padding: 10px 5px;
             border-style: solid;
             border-width: 1px;
             overflow: hidden;
             word-break: normal;
             border-color: #ccc;
-            color: #250b33;
+            color: #333;
             background-color: #fff;
         }
 
@@ -44,7 +44,7 @@
             overflow: hidden;
             word-break: normal;
             border-color: #ccc;
-            color: #e30f2c;
+            color: #333;
             background-color: #f0f0f0;
         }
 
@@ -54,23 +54,188 @@
         .center {
             text-align: center;
         }
+
+        .style_text{
+            width: auto;
+            font-size: 14px;
+            padding: 6px 0 4px 10px;
+            background: #F6F6f6;
+
+        }
+        .td_report {
+            border-bottom: 1px solid #ccc;
+            color: #669;
+            padding: 9px 8px;
+            transition: .3s linear;
+        }
+        .bot3:hover, .bot3:focus {
+            border-color: rgba(255, 0, 0, 0.8);
+            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px rgba(250, 0, 0, 0.9);
+            outline: 0 none;
+        }
+        .tf {
+            background-color: #FFFFFF;
+            border: 1px solid #CCCCCC;
+            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset;
+            transition: border 0.2s linear 0s, box-shadow 0.2s linear 0s;
+            border-radius: 4px;
+            color: #555555;
+            width: auto;
+            margin: auto;
+            font-size: 14px;
+            text-align: center;
+            height: auto;
+            line-height: 20px;
+            margin-bottom: 10px;
+            padding: 4px 6px;
+            vertical-align: middle;
+            text-decoration: none;
+        }
+
+        .tf:hover, .tf:focus {
+            border-color: rgba(12, 123, 69, 0.8);
+            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px rgba(12, 123, 69, 0.9);
+            outline: 0 none;
+        }
+
+
+
+
     </style>
 </head>
 <body>
 
-<h1>Book List</h1>
+<h1>Библиотека</h1>
+
+
+<c:if test="${!empty book.title}">
+    <h1>Редактировать книгу:</h1>
+</c:if>
+<c:if test="${empty book.title}">
+    <h1>Добавить книгу:</h1>
+</c:if>
+<c:url var="add" value="/books/add"/>
+<form:form action="${add}" commandName="book">
+    <table align="right">
+        <c:if test="${!empty book.title}">
+            <tr>
+                <td>
+                    <form:label path="id">
+                    </form:label>
+                </td>
+                <td>
+                    <form:input path="ide" readonly="true" disabled="true" type="hidden"/>
+                    <form:hidden path="id"/>
+                </td>
+            </tr>
+        </c:if>
+        <tr>
+            <td>
+                <form:label path="Title">
+                    <spring:message text="Название"/>
+                </form:label>
+            </td>
+            <td>
+                <form:input path="title"/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <form:label path="Description">
+                    <spring:message text="Описание"/>
+                </form:label>
+            </td>
+            <td>
+                <form:input path="description"/>
+            </td>
+        </tr>
+        <tr>
+        <tr>
+            <td>
+                <form:label path="Author">
+                    <spring:message text="Автор"/>
+                </form:label>
+            </td>
+            <td>
+                <form:input path="author"/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <form:label path="isbn">
+                    <spring:message text="ISBN"/>
+                </form:label>
+            </td>
+            <td>
+                <form:input path="isbn"/>
+            </td>
+        </tr>
+        <tr>
+        <tr>
+            <td>
+                <form:label path="printYear">
+                    <spring:message text="Год"/>
+                </form:label>
+            </td>
+            <td>
+                <form:input path="printYear"/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <form:label path="readAlready">
+                    <spring:message text="Год"/>
+                </form:label>
+            </td>
+            <td>
+                <form:input path="readAlready"/>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <c:if test="${!empty book.title}">
+                    <input type="submit"
+                           value="<spring:message text="Edit Book"/>"/>
+                </c:if>
+                <c:if test="${empty book.title}">
+                    <input type="submit"
+                           value="<spring:message text="Add Book"/>"/>
+                </c:if>
+            </td>
+        </tr>
+    </table>
+</form:form>
+
+<form action="/search/title">
+    Название: <input type="text" name="title" class="style_text"/>
+    <input type="submit" value="Поиск" class="tf"/>
+</form>
+<form action="/search/year">
+    Год: <input type="text" name="year" class="style_text"/>
+    <input type="submit" value="Поиск" class="tf"/>
+</form>
+<form action="/search/read">
+    Прочтено: <input type="text" name="read" class="style_text"/>
+    <input type="submit" value="Поиск" class="tf"/>
+</form>
+<form action="/">
+    <input type="submit" value="Сброс" class="tf"/>
+</form>
+
+
+
 
 <c:if test="${!empty listBooks}">
     <table class="tg">
         <tr>
-            <th width="120">Title</th>
-            <th width="100">Description</th>
-            <th width="120">Author</th>
-            <th width="40">ISBN</th>
-            <th width="30">PrintYear</th>
-            <th width="30">ReadAlready</th>
-            <th width="30">Edit</th>
-            <th width="30">Delete</th>
+            <th width="120">Название</th>
+            <th width="100">Описание</th>
+            <th width="100">Автор</th>
+            <th width="50">ISBN</th>
+            <th width="30">Год</th>
+            <th width="30">Прочтена</th>
+            <th width="30">Изменить</th>
+            <th width="30">Удалить</th>
 
         </tr>
         <c:forEach items="${listBooks}" var="book">
@@ -81,12 +246,21 @@
                 <td>${book.isbn}</td>
                 <td>${book.printYear}</td>
                 <td>${book.readAlready}</td>
-                <td><a href="<c:url value='/edit/${book.id}'/>">Edit</a></td>
-                <td><a href="<c:url value='/remove/${book.id}'/>">Delete</a></td>
+                <td class="td_report">
+                    <form action="/edit/${book.id}">
+                        <input type="submit" value="Изменить" class="bot3">
+                    </form>
+                </td>
+                <td class="td_report">
+                    <form action="/delete/${book.id}">
+                        <input type="submit" value="Удалить" class="bot3">
+                    </form>
+                </td>
             </tr>
         </c:forEach>
     </table>
 </c:if>
+
 
 
 <div class="center">
